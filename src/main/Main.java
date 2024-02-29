73bba14fae2c92d4f505a4286a246fe245c164d2
gokef23 <104698332+gokef23@users.noreply.github.com>
2024-02-29 06:41:37 -0300

    Task Manager v.0.0.1
    -Created class Task (primitive version)

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import model.Task;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Task t = new Task();
+		t.addTask();
+		System.out.println(t.toString());
+	}
+
+}
---
+++ src/model/Task.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task {
+
+//	private Integer id; // Id of the task
+	private String name; // Name of the task
+	private boolean state; // If it's true, the task is completed, otherwise it's false
+	private String description; // Description of the task
+	private Date dueDate; // Due date of the task
+
+	// Getters & Setters
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+//	public Integer getId() {
+//		return id;
+//	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	// Actions
+	public void addTask() {
+		Scanner sc = new Scanner(System.in);
+		String name, descr;
+		Date dueDateTask = null;
+		System.out.println("Enter the name of the task:");
+		name = sc.nextLine();
+		System.out.println("Enter the task description:");
+		descr = sc.nextLine();
+
+		System.out.println("Enter the due date of the task: (dd-MM-yyyy)");
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+
+		String dateInString = sc.nextLine();
+
+		try {
+			dueDateTask = formatter.parse(dateInString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sc.close();
+		this.setName(name);
+		this.setDescription(descr);
+			this.setDueDate(dueDateTask);
+	}
+
+	@Override
+	public String toString() {
+		return "Task [name=" + name + ", description=" + description + ", dueDate=" + dueDate + "]";
+	}
+	
+}
